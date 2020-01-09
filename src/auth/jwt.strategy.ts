2b655a68706689262fa7de81e2bf4eb71a090e8f
Petar Vujovic <petarvujovic98@gmail.com>
2020-01-09 09:11:24 +0100

    Get jwt strategy configuration from config service

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
-import { ExtractJwt, Strategy } from 'passport-jwt'
+import { ConfigService } from 'nestjs-config'
+import { Strategy } from 'passport-jwt'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
-    super({
-      jwtFromRequest: (req: { cookies: { jwt: string } }) =>
-        req && req.cookies ? req.cookies.jwt : null,
-      ignoreExpiration: false,
-      secretOrKey: 1,
-    })
+  constructor(private readonly configService: ConfigService) {
+    super(configService.get('jwt'))
   }
 
   async validate(payload: any) {
