b5339da1ce318283019ebdb02df49a5d2ca3b043
Petar Vujovic <petarvujovic98@gmail.com>
2020-01-12 15:34:45 +0100

    Make richer context user object from jwt

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-jwt'
+import { ContextUser } from 'src/dto/contextUser.object'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
-    super({
-      jwtFromRequest: (req: { cookies: { access_token: string } }) =>
-        req && req.cookies ? req.cookies.access_token : null,
-      ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
-    })
+  constructor(configService: ConfigService) {
+    super(configService.get('jwt.passport'))
   }
 
-  async validate(payload: any) {
-    return {
-      userId: payload.sub,
-      username: payload.username,
-      roles: payload.roles,
-    }
+  async validate(payload: any): Promise<ContextUser> {
+    return new ContextUser(
+      payload.sub,
+      payload.username,
+      payload.email,
+      payload.firstName,
+      payload.lastName,
+      payload.roles,
+    )
   }
 }
