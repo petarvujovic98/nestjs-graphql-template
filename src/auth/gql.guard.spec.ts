09383ded122dd23be38a817612e66dee351f2379
Petar Vujovic <petarvujovic98@gmail.com>
2020-01-12 22:12:02 +0100

    Add authorization and rename the guard

---
+++ src/auth/gql.guard.spec.ts
@@ -0,0 +1,7 @@
+import { GqlAuthGuard } from './gql.guard'
+
+describe('GqLAuthGuard', () => {
+  it('should be defined', () => {
+    expect(new GqlAuthGuard()).toBeDefined()
+  })
+})
---
+++ src/auth/gql.guard.ts
@@ -0,0 +1,28 @@
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host'
+import { GqlExecutionContext } from '@nestjs/graphql'
+import { AuthGuard } from '@nestjs/passport'
+import { ContextUser } from '../dto/contextUser.object'
+
+@Injectable()
+export class GqlAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    const roles = new Reflector().get<string[]>('roles', context.getHandler())
+    if (!roles) {
+      return true
+    }
+    const request = this.getRequest(context)
+    const user: ContextUser = request.user
+    return (
+      super.canActivate(new ExecutionContextHost([request])) &&
+      user &&
+      user.roles &&
+      user.hasRole(roles)
+    )
+  }
+
+  getRequest(context: ExecutionContext) {
+    return GqlExecutionContext.create(context).getContext().req
+  }
+}
