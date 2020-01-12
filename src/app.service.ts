f5a7a83f8e130db57be50d666ebfccc600fe82de
Petar Vujovic <petarvujovic98@gmail.com>
2020-01-12 15:41:28 +0100

    Add more detialied comments

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,9 @@ import { Injectable, OnModuleInit } from '@nestjs/common'
 
 @Injectable()
 export class AppService implements OnModuleInit {
+  // TODO                 <-- declare your typeorm injections here
   onModuleInit() {
-    // TODO               <-- declare your typeorm injections here
+    // TODO               <-- initialize your typeorm injections here
   }
   // TODO                 <-- define service constructor and methods
 }
