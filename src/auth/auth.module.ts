f1cc73e2636b9e140a6f1fda3626b0cc9843b2de
Petar Vujovic <petarvujovic98@gmail.com>
2020-01-12 15:35:26 +0100

    Remove unneccessary module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common'
-import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { ConfigModule, ConfigService } from 'nestjs-config'
 import { JwtStrategy } from './jwt.strategy'
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: configService.get('JWT_EXPIRATION'),
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   providers: [JwtStrategy],
 })
 export class AuthModule {}
