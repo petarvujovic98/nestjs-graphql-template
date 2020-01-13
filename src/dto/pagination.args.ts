9bfe6281accd7032f127a2e01c939d42ae7841ed
Petar Vujovic <petarvujovic98@gmail.com>
2020-01-13 10:09:04 +0100

    Fix quotemark rules and add description

--- src/dto/pagination.args.ts
+++ src/dto/pagination.args.ts
@@ -3,12 +3,18 @@ import { ArgsType, Field, Int } from 'type-graphql'
 
 @ArgsType()
 export class PaginationArgs {
-  @Field(type => Int, { nullable: true })
+  @Field(type => Int, {
+    nullable: true,
+    description: 'Number of items to skip from query result',
+  })
   @IsInt({ message: 'Skip argument must be integer!' })
   @Min(1, { message: 'Skip must not be less then 0!' })
   skip?: number = 0
 
-  @Field(type => Int, { nullable: true })
+  @Field(type => Int, {
+    nullable: true,
+    description: 'Number of items to take from query result',
+  })
   @IsInt({ message: 'Take argument must be integer!' })
   @Min(1, { message: 'Take must not be less then 0!' })
   take?: number = 10
