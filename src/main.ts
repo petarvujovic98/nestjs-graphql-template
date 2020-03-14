9a208a8781acd637c9a6fa3e91419fa3a19ad0bc
Petar Vujovic <petarvujovic98@gmail.com>
2020-03-14 18:13:52 +0100

    Fix bootstrap config

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,37 @@
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import fastifyCompress = require('fastify-compress')
 import fastifyHelmet = require('fastify-helmet')
 import fastifyRateLimit = require('fastify-rate-limit')
+import fastifyCookie = require('fastify-cookie')
+import fastifyCors = require('fastify-cors')
 
 import { AppModule } from './app.module'
 
 declare const module: any
 
 async function bootstrap() {
-  const fastifyAdapter = new FastifyAdapter({ logger: true })
-  fastifyAdapter.register(fastifyCompress)
-  fastifyAdapter.register(fastifyHelmet)
-  fastifyAdapter.register(fastifyRateLimit, {
-    max: 100,
-    timeWindow: '1 minute',
-  })
-
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    fastifyAdapter,
+    new FastifyAdapter({ logger: { prettyPrint: true } }),
   )
+
+  const configService = app.get(ConfigService)
+
+  app.register(fastifyCompress)
+  app.register(fastifyHelmet)
+  app.register(fastifyRateLimit, {
+    max: configService.get('rateLimit.max'),
+    timeWindow: configService.get('rateLimit.timeWindow'),
+  })
+  app.register(fastifyCookie)
+  app.register(fastifyCors, { origin: configService.get('app.origins') })
 
   app.useGlobalPipes(new ValidationPipe())
 
-  const port = parseInt(process.env.PORT, 10)
-  await app.listen(port || 3000, '0.0.0.0')
+  await app.listen(configService.get('app.port') || 3000, '0.0.0.0')
 
   if (module.hot) {
     module.hot.accept()
